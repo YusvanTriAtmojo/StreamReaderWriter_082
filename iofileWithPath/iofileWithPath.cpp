05bd3f2125ff470dfe2aa85fc83b18a1d703fd9c
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-06-21 10:56:47 +0700

    Membuat prosedur main yang didalamnya terdapat variabel baris dan NamaFile. Lalu cout untuk memasukkan nama file dan memasukkannya dalam variabel NamaFile

---
+++ iofileWithPath/iofileWithPath.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukkan Nama File : ";
+	cin >> NamaFile;
+}
