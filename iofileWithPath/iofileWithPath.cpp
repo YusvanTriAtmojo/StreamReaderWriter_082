ff052cc44a19e527dcfcdf5ac603920ba64834a8
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-06-21 11:16:40 +0700

    Membuka file dengan nama yang dimasukkan kemudian dengan mode membaca. Jika nama file ada maka akan melakukan perulangan while untuk membaca data pada variabel baris dan menampilkannya. Setelah selesai maka file ditutup. Kemudian jika nama file tidak ada maka terdapat peringatan bahwa file tidak ada.

--- iofileWithPath/iofileWithPath.cpp
+++ iofileWithPath/iofileWithPath.cpp
@@ -30,4 +30,26 @@ int main() {
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+	//Membuka file dalam mode membaca 
+	ifstream infile;
+
+	//menunjuk ke sebuah file
+	infile.open(NamaFile, ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup dile tersebut setelah selesai
+		infile.close();
+	}
+	//Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
